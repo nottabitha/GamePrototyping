b51d9588c02faec30c96c979de3f609fb8169972
nottabitha <leimonistabitha@gmail.com>
2019-09-04 09:52:46 +1000

    Rope Halfway Done, Player Movement Done

---
+++ Ta-Neter Land of the Gods/Assets/Scripts/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+	public float speed  = 5; 
+	public bool isSwinging;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}
