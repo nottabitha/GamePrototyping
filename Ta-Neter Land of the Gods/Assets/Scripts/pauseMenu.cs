8a96c855d3f789a89049b212559b1bce4788403e
teletubbie4 <101612273@student.swin.edu.au>
2019-09-24 23:57:38 +1000

    Added Placeholder pause menu to level 2

---
+++ Ta-Neter Land of the Gods/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour {
+
+	public GameObject PauseUI;
+
+	private bool paused = false;
+
+	void Start() {
+	
+		PauseUI.SetActive(false);
+	
+	}
+
+	void Update() {
+
+		if(Input.GetButtonDown("Pause")) {
+
+			paused = !paused;
+
+		}
+
+		if(paused) {
+
+			PauseUI.SetActive(true);
+			Time.timeScale = 0;
+		}
+
+		if(!paused) {
+
+			PauseUI.SetActive(false);
+			Time.timeScale = 1;
+
+		}
+			
+	}
+
+	public void Resume() {
+
+		paused = false;
+
+	}
+
+	public void MainMenu() {
+
+		Application.LoadLevel(0);
+
+	}
+
+	public void Quit() {
+
+		Application.Quit();
+
+	}
+
+}
