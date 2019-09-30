b444e30587ceee96bafe618100b1debc1620d8ad
teletubbie4 <101612273@student.swin.edu.au>
2019-09-30 18:11:35 +1000

    Added Main Menu Placeholder w/ some functionality

---
+++ Ta-Neter Land of the Gods/Assets/Scripts/menuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class menuManager : MonoBehaviour
+{
+	public void StartGame() 
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void Quit() 
+	{
+		Application.Quit();
+	}
+}
