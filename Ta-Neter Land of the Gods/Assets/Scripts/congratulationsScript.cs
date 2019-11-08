3c7c295dc536f864f73341ef95ec3bbbab4d1b7d
teletubbie4 <101612273@student.swin.edu.au>
2019-11-08 13:00:49 +1100

    Added congratulations UI to final level

---
+++ Ta-Neter Land of the Gods/Assets/Scripts/congratulationsScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class congratulationsScript : MonoBehaviour
+{
+    public GameObject congratulationsUI;
+    private bool congratulations = false;
+    private health anubisHealth;
+    public GameObject anubis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anubisHealth = anubis.GetComponent<health>();
+        congratulationsUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+			if (anubisHealth.Health <= 0)
+			{
+				congratulations = true;
+			}
+
+			if (congratulations)
+			{
+				congratulationsUI.SetActive(true);
+				Time.timeScale = 0;
+			}
+    }
+}
---
+++ Ta-Neter Land of the Gods/Assets/Scripts/creditsScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditsScript : MonoBehaviour
+{
+    public void Credits()
+    {
+        Application.LoadLevel(5);
+    }
+}
